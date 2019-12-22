61c596e73d97af8adb85c25c2c9f315cd6d345af
Ibr <ihasan@futureit-eg.com>
2019-12-22 14:09:16 +0200

    Adding DB + enhancments

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const env = require('dotenv');
 const logger = require('morgan');
+const colors = require('colors');
+const connectDB = require('./config/db');
+
+// Load env variables
+env.config({ path: '.env' });
+
+// Connect to DB
+connectDB();
 
 // Route files
 const compaines = require('./routes/companies');
 
-// Load env variables
-env.config({ path: '.env' });
 
 const app = express();
 
@@ -20,6 +26,13 @@ app.use('/api/v1/companies', compaines);
 
 let port = process.env.PORT || 5000;
 
-app.listen(port, () => {
-    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port}`);
-})
+const server = app.listen(port, () => {
+    console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${port}`.yellow.bold);
+});
+
+// Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`.red);
+    // Close server and exit process
+    server.close(() => process.exit(1));
+});
