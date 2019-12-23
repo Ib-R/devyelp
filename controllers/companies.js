89620fbfc0655e3f923f0b67c585056fe0d63fc0
Ibr <ihasan@futureit-eg.com>
2019-12-23 18:23:39 +0200

    Pagination

--- controllers/companies.js
+++ controllers/companies.js
@@ -7,8 +7,60 @@ const Company = require('../models/Company');
 // @route   GET /api/v1/companies
 // @access  Public
 exports.getCompanies = asyncHandler(async (req, res, next) => {
-    const companies = await Company.find();
-    res.json({ success: true, count: companies.length, data: companies });
+    const reqQuery = { ...req.query };
+
+    // Queries to Exclude
+    const removeQueries = ['select', 'sort', 'page', 'limit'];
+    removeQueries.forEach(param => delete reqQuery[param]);
+
+    // Filtering
+    let queryStr = JSON.stringify(reqQuery);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
+    query = Company.find(JSON.parse(queryStr));
+
+    // Select fields
+    if (req.query.select) {
+        const fields = req.query.select.split(',').join(' ');
+        query = query.select(fields);
+    }
+
+    // Sort
+    if (req.query.sort) {
+        query = query.sort(req.query.sort);
+    } else {
+        query = query.sort('-createdAt');
+    }
+
+    // Pagination
+    const page = parseInt(req.query.page, 10) || 1;
+    const limit = parseInt(req.query.limit, 10) || 10;
+    const startIndex = (page - 1) * limit;
+    const endIndex = page * limit;
+    const total = await Company.countDocuments();
+
+    query = query.skip(startIndex).limit(limit);
+
+    // Executing query
+    const companies = await query;
+
+    // Pagination result
+    const pagination = {};
+
+    if (endIndex < total) { // if not the last page
+        pagination.next = {
+            page: page + 1,
+            limit
+        };
+    }
+
+    if (startIndex > 0) { // if not first page
+        pagination.prev = {
+            page: page - 1,
+            limit
+        };
+    }
+
+    res.json({ success: true, count: companies.length, pagination, data: companies });
 });
 
 // @desc    Create new companies
